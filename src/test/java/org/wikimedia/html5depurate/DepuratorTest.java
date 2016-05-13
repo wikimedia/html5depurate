6d02dc032f18605ebac4526a0a7fe96d64abb532
Tim Starling <tstarling@wikimedia.org>
2016-05-13 15:53:31 +1000

    Fix three p-wrapping bugs
    * Don't clear the p-wrapper's blank flag when closing it due to the
      appearance of a block-level start tag. This is the Kefteji test case
      on https://www.mediawiki.org/wiki/Parsing/Replacing_Tidy?oldid=2126601
    * When creating a new p-wrapper due to the appearance of an inline-level
      start tag, immediately clear the p-wrapper's blank flag.
    * Be like Tidy when p-wrapping comments, don't clear the blank
      flag. Originally we cleared the blank flag because Tidy does not
      delete empty elements with comments in them, so it made sense to not
      put class="mw-empty-li" on such elements. We can either have two flags
      (one for p-wrapping and one for mw-empty-li), or pick a winner. I'm
      picking p-wrapping as the winner since mw-empty-li is really our
      invention, not Tidy's, so we can write the spec.
    
    Add associated tests and bump version.
    
    Change-Id: Ibdd4dbc6058753452ac3e0daaf3b99e847743021

---
+++ src/test/java/org/wikimedia/html5depurate/DepuratorTest.java
@@ -0,0 +1,81 @@
+package org.wikimedia.html5depurate;
+
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collection;
+import org.xml.sax.InputSource;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class DepuratorTest {
+	private String input;
+	private String expected;
+	private boolean compat;
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		final boolean COMPAT = true;
+		final boolean NOCOMPAT = false;
+
+		return Arrays.asList(new Object[][] {
+			// Tests are indexed for cross-referencing in junit output
+			// 0. Empty string
+			{COMPAT, "", ""},
+			// 1. Simple p-wrap
+			{COMPAT, "x", "<p>x</p>"},
+			// 2. No p-wrap of blank node
+			{COMPAT, " ", " "},
+			// 3. p-wrap terminated by div
+			{COMPAT, "x<div></div>", "<p>x</p><div></div>"},
+			// 4. p-wrap not terminated by span
+			{COMPAT, "x<span></span>", "<p>x<span></span></p>"},
+			// 5. An element is non-blank and so gets p-wrapped
+			{COMPAT, "<span></span>", "<p><span></span></p>"},
+			// 6. The blank flag is set after a block-level element
+			{COMPAT, "<div></div> ", "<div></div> "},
+			// 7. Blank detection between two block-level elements
+			{COMPAT, "<div></div> <div></div>", "<div></div> <div></div>"},
+			// 8. But p-wrapping of non-blank content works after an element
+			{COMPAT, "<div></div>x", "<div></div><p>x</p>"},
+			// 9. p-wrapping between two block-level elements
+			{COMPAT, "<div></div>x<div></div>", "<div></div><p>x</p><div></div>"},
+			// 10. p-wrap inside blockquote
+			{COMPAT, "<blockquote>x</blockquote>", "<blockquote><p>x</p></blockquote>"},
+			// 11. A comment is blank for p-wrapping purposes
+			{COMPAT, "<!-- x -->", "<!-- x -->"},
+			// 12. A comment is blank even when a p-wrap was opened by a text node
+			{COMPAT, " <!-- x -->", " <!-- x -->"},
+			// 13. A comment does not open a p-wrap
+			{COMPAT, "<!-- x -->x", "<!-- x --><p>x</p>"},
+			// 14. A comment does not close a p-wrap
+			{COMPAT, "x<!-- x -->", "<p>x<!-- x --></p>"},
+			// 15. Empty li
+			{COMPAT, "<ul><li></li></ul>", "<ul><li class=\"mw-empty-li\"></li></ul>"},
+		});
+	}
+
+	public DepuratorTest(boolean compat_, String input_, String expected_) {
+		compat = compat_;
+		input = input_;
+		expected = expected_;
+	}
+
+	@Test
+	public void test() throws Exception {
+		String prefix = "<html><head></head><body>";
+		String suffix = "</body></html>";
+		StringReader sr = new StringReader(prefix + input + suffix);
+		InputSource source = new InputSource(sr);
+		byte[] buffer = Depurator.depurate(source, compat);
+		String html = new String(buffer, Charset.forName("UTF-8"));
+		Assert.assertTrue(html.startsWith(prefix));
+		Assert.assertTrue(html.endsWith(suffix));
+		Assert.assertEquals(expected, html.substring(prefix.length(), html.length() - suffix.length()));
+	}
+}
